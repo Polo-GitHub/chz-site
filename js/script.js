0875c8fe6fef5e32b707ee1c7a9bd2cf1fdfdf7c
Polo-GitHub <149659299+Polo-GitHub@users.noreply.github.com>
2024-12-23 19:14:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,5 +19,18 @@ const userNameInput = document.querySelector(".user__input");
 const userPasswordInput = document.querySelector(".user-input");
 const userNameErr = document.querySelector(".err__mssg");
 const userPasswordErr = document.querySelector(".user__err");
-const labelUserError = document.querySelector(".user__label")
-const labelPasswdErr = document.querySelector(".user-label")
+const labelUserError = document.querySelector(".user__label");
+const labelPasswdErr = document.querySelector(".user-label");
+
+// Show error message
+function showError(element, elementError){
+    element.classList.add("error__border");
+    elementError.style.display = "block";   
+}
+// Clear error message
+function clearError(element, elementError){
+    element.classList.remove("error__border");
+    elementError.style.display = "none";   
+}
+
+
