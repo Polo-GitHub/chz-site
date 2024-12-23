0383d11c278423bd3b335bc0981f13374f43ff22
Polo-GitHub <149659299+Polo-GitHub@users.noreply.github.com>
2024-12-23 00:02:42 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,11 @@ toggleBtn.addEventListener("click", ()=>{
     }
 
 });
+// Form Validation
+const formBody = document.querySelector(".form__container");
+const userNameInput = document.querySelector(".user__input");
+const userPasswordInput = document.querySelector(".user-input");
+const userNameErr = document.querySelector(".err__mssg");
+const userPasswordErr = document.querySelector(".user__err");
+const labelUserError = document.querySelector(".user__label")
+const labelPasswdErr = document.querySelector(".user-label")
