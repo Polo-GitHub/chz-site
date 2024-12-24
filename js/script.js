c8b34f730079d080798b58935776e4da16d13083
Polo-GitHub <149659299+Polo-GitHub@users.noreply.github.com>
2024-12-24 12:55:51 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,24 +13,27 @@ toggleBtn.addEventListener("click", ()=>{
     }
 
 });
-// Form Validation
-const formBody = document.querySelector(".form__container");
-const userNameInput = document.querySelector(".user__input");
-const userPasswordInput = document.querySelector(".user-input");
-const userNameErr = document.querySelector(".err__mssg");
-const userPasswordErr = document.querySelector(".user__err");
-const labelUserError = document.querySelector(".user__label");
-const labelPasswdErr = document.querySelector(".user-label");
-
-// Show error message
-function showError(element, elementError){
-    element.classList.add("error__border");
-    elementError.style.display = "block";   
 }
 // Clear error message
-function clearError(element, elementError){
+    function clearError(element, elementError){
     element.classList.remove("error__border");
     elementError.style.display = "none";   
 }
 
+    formBody.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    if(userNameInput.value.trim() === ''){
+        showError(userNameInput, userNameErr)
+        userNameInput.style.borderColor = "#bf2155"
+        labelUserError.style.color = "#bf2155"
+       
+    }
+    if(userPasswordInput.value.trim() === ''){
+        showError(userPasswordInput, userPasswordErr)
+        userPasswordInput.style.borderColor = "#bf2155"
+        labelPasswdErr.style.color = "#bf2155"  
+    }
+});
+  
 
